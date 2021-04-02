0fe63c6fab6126dabb7a5fd198a4f50826848a31
Stanislav Yankovskiy <stanislav.yankovskiy@sirenltd.com>
2021-04-02 12:10:09 +0300

    create MainController

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -7,7 +7,12 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-class Controller extends BaseController
+class MainController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function home()
+    {
+        return 'home';
+    }
 }
