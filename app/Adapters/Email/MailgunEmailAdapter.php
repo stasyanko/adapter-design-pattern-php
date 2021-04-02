06fee44b9605caf89019e847a423b7c204903509
Stanislav Yankovskiy <stanislav.yankovskiy@sirenltd.com>
2021-04-02 13:21:56 +0300

    raise php version to 7.4
    update MailgunEmailAdapter

--- app/Adapters/Email/MailgunEmailAdapter.php
+++ app/Adapters/Email/MailgunEmailAdapter.php
@@ -1,14 +1,34 @@
 <?php
 
-
 namespace App\Adapters\Email;
 
+use Mailgun\Mailgun;
 
 class MailgunEmailAdapter implements EmailAdapter
 {
+    private string $apiKey;
+    private string $domain;
+    private string $fromAddress;
+
+    public function __construct(
+        string $apiKey,
+        string $domain,
+        string $fromAddress
+    )
+    {
+        $this->apiKey = $apiKey;
+        $this->domain = $domain;
+        $this->fromAddress = $fromAddress;
+    }
 
     public function send(string $toAddress, string $subject, string $message): void
     {
-        // TODO: Implement send() method.
+        $mg = Mailgun::create($this->apiKey);
+        $mg->messages()->send($this->domain, [
+            'from'    => $this->fromAddress,
+            'to'      => $toAddress,
+            'subject' => $subject,
+            'text'    => $message,
+        ]);
     }
 }
